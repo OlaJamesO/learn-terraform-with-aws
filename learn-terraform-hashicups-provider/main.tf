d7e59481d96ae52dc83d6aed20773901956f3fe7
jokuso01 <okusolubo@gmail.com>
2022-03-22 03:30:24 +0000

    terrafrom with docker compose

---
+++ learn-terraform-hashicups-provider/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    hashicups = {
+      source = "hashicorp/hashicups"
+      version = "0.3.1"
+    }
+  }
+}
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+}
+
+resource "hashicups_order" "edu" {
+  items {
+    coffee {
+      id = 3
+    }
+    quantity = 3
+  }
+  items {
+    coffee {
+      id = 2
+    }
+    quantity = 1
+  }
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
+}
+
+data "hashicups_ingredients" "first_coffee" {
+  coffee_id = hashicups_order.edu.items[0].coffee[0].id
+}
+
+output "first_coffee_ingredients" {
+  value = data.hashicups_ingredients.first_coffee
+}
